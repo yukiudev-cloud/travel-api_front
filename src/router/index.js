20fbfc57a1259878564e4689e144d92b12d2e055
“yuki.u” <yuki.u.dev@gmail.com>
2026-04-29 02:29:37 +0900

    oomojikomoji

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router"
 
 const routes = [
-  { path: "/", component: () => import("../pages/Home.vue") },
-  { path: "/privacy", component: () => import("../pages/Privacy.vue") },
-  { path: "/contact", component: () => import("../pages/Contact.vue") }
+  { path: "/", component: () => import("../pages/home.vue") },
+  { path: "/privacy", component: () => import("../pages/privacy.vue") },
+  { path: "/contact", component: () => import("../pages/contact.vue") }
 ]
 
 const router = createRouter({

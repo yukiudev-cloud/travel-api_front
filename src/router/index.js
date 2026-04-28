041d38439a08474212db8ee817570ac01e38b705
“yuki.u” <yuki.u.dev@gmail.com>
2026-04-29 02:39:41 +0900

    root

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 
 const routes = [
   { path: "/", component: () => import("../pages/home.vue") },
@@ -7,8 +7,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory("/travel-api_front/"),
-  routes,
+  history: createWebHashHistory(),
+  routes: routes
 })
 
 export default router
